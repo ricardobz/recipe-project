ebe08a7bc5ec926e124a1c14ae8ea8f9d0569940
Ricardo Beckert <rbeckert@esss.co>
2023-05-22 11:17:48 -0300

    Add recipe list items

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Cookies',
+      'This is a Simple Cookie',
+      'https://bakesbybrownsugar.com/wp-content/uploads/2023/04/Easter-Chocolate-Chip-Cookies-24.jpg'
+    ),
+    new Recipe(
+      'Brownie',
+      'This is a Simple Brownie',
+      'https://handletheheat.com/wp-content/uploads/2017/03/Chewy-Brownies-Square-1.jpg'
+    ),
+  ];
 }
