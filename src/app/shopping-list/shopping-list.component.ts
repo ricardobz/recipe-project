5d394b329bc613da9f240e25ce026abea17fd012
Ricardo Beckert <rbeckert@esss.co>
2023-05-29 09:56:51 -0300

    Add shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Milk', 2),
+    new Ingredient('Flour', 2),
+    new Ingredient('Sugar', 1),
+    new Ingredient('Chocolate', 4),
+  ];
 }
