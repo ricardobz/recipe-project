8b5e0aa39144d2bff54c817cfcc384a12aa52a0f
Ricardo Beckert <rbeckert@esss.co>
2023-07-03 10:31:14 -0300

    Add ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
@@ -8,12 +9,26 @@ export class RecipeService {
     new Recipe(
       'Cookies',
       'This is a Simple Cookie',
-      'https://bakesbybrownsugar.com/wp-content/uploads/2023/04/Easter-Chocolate-Chip-Cookies-24.jpg'
+      'https://bakesbybrownsugar.com/wp-content/uploads/2023/04/Easter-Chocolate-Chip-Cookies-24.jpg',
+      [
+        new Ingredient('Milk', 1),
+        new Ingredient('Flour', 1),
+        new Ingredient('Sugar', 1),
+        new Ingredient('Chocolate', 2),
+      ]
     ),
     new Recipe(
       'Brownie',
       'This is a Simple Brownie',
-      'https://handletheheat.com/wp-content/uploads/2017/03/Chewy-Brownies-Square-1.jpg'
+      'https://handletheheat.com/wp-content/uploads/2017/03/Chewy-Brownies-Square-1.jpg',
+      [
+        new Ingredient('Egg', 1),
+        new Ingredient('Butter', 1),
+        new Ingredient('Flour', 1),
+        new Ingredient('Sugar', 1),
+        new Ingredient('Chocolate', 2),
+        new Ingredient('Dark Chocolate', 1),
+      ]
     ),
   ];
 
